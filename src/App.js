a113e38c45e51c8c04f943bd3db49a6984010d6b
orinae26 <oryna.rich26@gmail.com>
2022-04-26 12:33:43 +0300

    Function/Simple component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+
+
+function App (){
+    const element = (
+        <div>
+            <h1>Hello React!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}. </h2>
+        </div>
+    )
+    return element;
 }
-
+setInterval(App, 1000)
 export default App;
